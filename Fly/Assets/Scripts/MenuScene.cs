fff1295ae39eb06f5c5ed001a3cdb95b62b0d6af
Vishal Jangid <vishaljangid2@outlook.com>
2019-12-13 14:48:22 +0530

    Preloader Scene

---
+++ Fly/Assets/Scripts/MenuScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScene : MonoBehaviour
+{
+    private CanvasGroup fadeGroup;
+    private float fadeInSpeed = 0.4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fadeGroup = FindObjectOfType<CanvasGroup>(); // Grabbing only canvas group      
+        fadeGroup.alpha = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fadeGroup.alpha = 1 - Time.timeSinceLevelLoad * fadeInSpeed;
+        
+    }
+
+    public void OnPlayBtnClick()
+    {
+        Debug.Log("Play Button Clicked");
+    }
+}
---
+++ Fly/Assets/Scripts/Preloader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Preloader : MonoBehaviour
+{
+    private CanvasGroup fadeGroup;
+    private float loadTime;
+    private float minimumLogoTime = 3.0f;
+    
+      
+    void Start()
+    {
+        fadeGroup = FindObjectOfType<CanvasGroup>(); // Grabbing only canvas group        
+
+        // If have to download 
+        if (Time.time < minimumLogoTime)
+            loadTime = minimumLogoTime;
+        else
+            loadTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Fade in
+        if (Time.time < minimumLogoTime)
+        {
+            fadeGroup.alpha = 1 - Time.time;
+        }
+
+        // fade out 
+        if(Time.time > minimumLogoTime && loadTime != 0)
+        {
+            fadeGroup.alpha = Time.time - minimumLogoTime;
+            if (fadeGroup.alpha >= 1) 
+            {
+                // Loading Main Menu
+                UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+            }
+        }
+    }
+}
